1eca9b48d58653b3b22328b1b97c76664d70ff2a
Victor Wesley <victor.infowb@gmail.com>
2025-07-21 08:45:53 -0300

    feat: Criando as rotas MissoesController com mock

---
+++ src/main/java/dev/java10x/cadastroDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,30 @@
+package dev.java10x.cadastroDeNinjas.Missoes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissoesController {
+
+    @GetMapping("/listar")
+    public String ListarMissoes(){
+        return "Missao listadas com sucesso";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missao criada com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao alterada com sucesso";
+    }
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao deletada com sucesso";
+    }
+
+
+
+}
