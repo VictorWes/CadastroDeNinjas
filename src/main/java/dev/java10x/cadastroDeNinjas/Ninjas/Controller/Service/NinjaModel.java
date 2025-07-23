e1a5adae1851c7e9e0f129d15450082c668223be
Victor Wesley <victor.infowb@gmail.com>
2025-07-23 17:55:30 -0300

    fix: Inclui a coluna ranking na minha entidade NinjaModel, porem já estavam incluida na migration V2

--- src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
@@ -34,5 +34,55 @@ public class NinjaModel {
      @JoinColumn(name = "missoes_id")
      private MissoesModel missoes;
 
+     @Column(name = "ranking")
+     private String ranking;
 
+
+     public Long getId() {
+          return id;
+     }
+
+     public void setId(Long id) {
+          this.id = id;
+     }
+
+     public String getNome() {
+          return nome;
+     }
+
+     public void setNome(String nome) {
+          this.nome = nome;
+     }
+
+     public String getEmail() {
+          return email;
+     }
+
+     public void setEmail(String email) {
+          this.email = email;
+     }
+
+     public int getIdade() {
+          return idade;
+     }
+
+     public void setIdade(int idade) {
+          this.idade = idade;
+     }
+
+     public MissoesModel getMissoes() {
+          return missoes;
+     }
+
+     public void setMissoes(MissoesModel missoes) {
+          this.missoes = missoes;
+     }
+
+     public String getRanking() {
+          return ranking;
+     }
+
+     public void setRanking(String ranking) {
+          this.ranking = ranking;
+     }
 }
