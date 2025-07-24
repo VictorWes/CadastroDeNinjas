277797666a6421d4db5382f932fd9d222bd60709
Victor Wesley <victor.infowb@gmail.com>
2025-07-23 21:36:40 -0300

    feat: adiciona DTO e mapper para missoes

--- src/main/java/dev/java10x/cadastroDeNinjas/Missoes/MissoesDTO.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Missoes/MissoesDTO.java
@@ -1,4 +1,54 @@
 package dev.java10x.cadastroDeNinjas.Missoes;
 
+import dev.java10x.cadastroDeNinjas.Ninjas.Controller.Service.NinjaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MissoesDTO {
+
+    private Long id;
+
+    private String nome;
+
+    private String dificuldade;
+
+    private List<NinjaModel> ninjas;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(String dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+
+    public List<NinjaModel> getNinjas() {
+        return ninjas;
+    }
+
+    public void setNinjas(List<NinjaModel> ninjas) {
+        this.ninjas = ninjas;
+    }
 }
